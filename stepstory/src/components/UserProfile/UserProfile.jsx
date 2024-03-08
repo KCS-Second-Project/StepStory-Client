e2c466bb2f9409f7f5516905da3d153793622e50
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-08 22:20:43 +0900

    feat: 프로필 더미 이미지 삽입

--- stepstory/src/components/UserProfile/UserProfile.jsx
+++ stepstory/src/components/UserProfile/UserProfile.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './style.css';
-import ImageOptionButton from '../Buttons/ImageOptionButton/ImageOptionButton';
 
-export default function UserProfile({ user, date }) {
+export default function UserProfile({ image, user, date }) {
   return (
     <div className="user-profile">
-      <div className="user-profile-picture"></div>
+      <div className="user-profile-picture">
+        <img className="profile-img" src={image} alt="user profile" />
+      </div>
       <div className="user-profile-name">{user}</div>
       <div className="user-profile-date">{date}</div>
-      <ImageOptionButton className="option-button"/>
     </div>
   );
 } 
