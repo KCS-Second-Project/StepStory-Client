b4fb4b8363143d21dafc205bb22d96e4a9f58094
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-08 23:21:51 +0900

    feat: 댓글 컴포넌트에 '답글 달기' 버튼 추가

--- stepstory/src/components/Comment/Comment.jsx
+++ stepstory/src/components/Comment/Comment.jsx
@@ -7,6 +7,10 @@ export default function Comment({ image, user, date, content }) {
       <div className="comment">
         <UserProfile image={image} user={user} date={date} />
         <p className="comment-body">{content}</p>
+        <div className="reply-container">
+            <button className="reply-button">답글 달기</button>
+        </div>
       </div>
+
     );
   }
