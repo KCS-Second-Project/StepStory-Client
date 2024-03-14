dce4a699c62afff9f00ad8d6daa1f0de6240cbe4
MeangSoong <149292274+MeangSoong@users.noreply.github.com>
2024-03-14 18:13:31 +0900

    feat: BestPost Component 생성 및 더미 데이터 대표이미지 생성

--- stepstory/src/App.js
+++ stepstory/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import React from 'react';
 import ViewPostLayout from './components/PageLayout/ViewPostLayout';
 import profileImg from './assets/images/defaultprofile.png';
-import StepmateList from './components/StepmateList/StepmatesList';
-import StepmatesRequetList from './components/StepmateRequestList/StepmateRequestList';
+import BestPost from './components/BestPost/BestPost';
+
+
 
 function App() {
   return (
     <div className="App">
-      <StepmatesRequetList />
+      <BestPost 
+                profile={profileImg} 
+                username={'Username'} 
+                title={'test'} 
+                wantToGoCount={999} 
+                date={'2024-03-01 ~ 20240-03-11'} 
+                place={'가산디지털단지'}
+            />
     </div>
   );
 }
