5612b41d8323ef632212b4c04d26a0c5dba43640
MeangSoong <149292274+MeangSoong@users.noreply.github.com>
2024-03-14 17:30:27 +0900

    feat: Stepmate Button 생성 및 이미지 추가

--- stepstory/src/App.js
+++ stepstory/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
 import ViewPostLayout from './components/PageLayout/ViewPostLayout';
+import StepmatesButton from './components/Buttons/StepmatesButton/StepmatesButton';
 
 function App() {
   return (
     <div className="App">
-      <ViewPostLayout />
+      <StepmatesButton />
     </div>
   );
 }
