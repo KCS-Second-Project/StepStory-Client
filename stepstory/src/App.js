a62600b91fb470a9a826f3e40d02110c22c78086
HyeIn <helen6662@naver.com>
2024-03-17 15:35:45 +0900

    feat: 기본 이미지 및 요소 삽입

--- stepstory/src/App.js
+++ stepstory/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import logo from './image/logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {/*<header className="App-header">*/}
+        {/*  <img src={logo} className="App-logo" alt="logo" /> 로고 돌아가는 애니메이션*/}
+        {/*  <p>*/}
+        {/*    StepStory Main Screen*/}
+        {/*  </p>*/}
+        {/*  <a*/}
+        {/*    className="App-link"*/}
+        {/*    href="https://github.com/orgs/KCS-Second-Project/repositories"*/}
+        {/*    target="_blank"*/}
+        {/*    rel="noopener noreferrer"*/}
+        {/*  >*/}
+        {/*    Go Github Link*/}
+        {/*  </a>*/}
+        {/*</header>*/}
+        <header className="App-test">
+          <img src={logo} alt="logo" style={{width: '15%'}}/>
+          {/*<h1 style={{color: 'blue'}}>content</h1>*/}
+        </header>
+      </div>
   );
 }
 
--- stepstory/src/index.js
+++ stepstory/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+
+import Login from './components/Login/login.jsx'; // Login 컴포넌트를 import 해야 합니다.
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PostStep1 from "./components/PostStep1/step1";
+import Socialsign from "./components/SocialSign/socialsign";
+import Sign from "./components/Sign/sign";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Login />
+        {/*<Socialsign />*/}
+        {/*<Sign />*/}
+        {/*<PostStep1 />*/}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
