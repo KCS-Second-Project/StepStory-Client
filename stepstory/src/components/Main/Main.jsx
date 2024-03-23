c7aa9a031c21086808e8474038ee0d211023dc03
HyeIn <helen6662@naver.com>
2024-03-24 03:00:11 +0900

    feat : Main페이지 내 MyStory 이동 버튼 추가

--- stepstory/src/components/Main/Main.jsx
+++ stepstory/src/components/Main/Main.jsx
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
 import logo from '../../image/Blogo.svg';
 import './Main.css';
 import Footer from '../Footer/footer';
 import MainMap from "../../components/MainMap/MainMap";
+import open from "../../image/OpenD.png"
+import close from "../../image/CloseD.png"
+
 
 // 왼쪽 화면 컴포넌트
 const MainLeftPane = () => {
+    const [isHovered, setIsHovered] = useState(false); // 버튼의 호버 상태를 추적하기 위한 상태 추가
+
     return (
         <div className="left-pane">
             <div className="left-content">
@@ -15,12 +21,20 @@ const MainLeftPane = () => {
                 </div>
             </div>
             <div className="logout-container">
+                <button className="story-btn"
+                        onMouseEnter={() => setIsHovered(true)} // 마우스가 버튼 위에 있을 때
+                        onMouseLeave={() => setIsHovered(false)} // 마우스가 버튼을 떠날 때
+                >
+                    {/* 버튼 상태에 따라 이미지 변경 */}
+                    <img src={isHovered ? open : close} alt="MyStory" className="story-icon" />
+                </button>
                 <button className="btn">Write Story&nbsp;&nbsp;✏️</button>
                 <button className="logout-btn">LOGOUT</button>
             </div>
         </div>
     );
 };
+
 
 // 오른쪽 화면 컴포넌트
 const MainRightPane = () => {
